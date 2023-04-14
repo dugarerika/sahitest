d6d304167edb800c04e6711e01024e21ffb713d3
Erika Tavera <dugarerika@gmail.com>
2023-04-14 17:18:59 +0400

    invetory changes

--- src/test/java/Steps/StepsInventorySection.java
+++ src/test/java/Steps/StepsInventorySection.java
@@ -1,7 +1,7 @@
 package Steps;
 
 
-import io.cucumber.datatable.DataTable;
+import io.cucumber.brachdatatable.DataTable;
 import io.cucumber.java.en.And;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
